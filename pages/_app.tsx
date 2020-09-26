67b9bb7ad10f602a3f8d2261e28cc25b16716fa9
nkrivous <kolsln@list.ru>
2020-09-27 02:11:38 +0300

    Added Sentry

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { theme } from "~/src/theme/theme";
+import { initSentry } from "~/src/sentry";
 import "~/src/theme/font-face.css";
 
 //import I18nProvider from "~/src/common/I18nProvider";
+
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  initSentry();
+}
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
